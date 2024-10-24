9f5c2945686585ee1de28470a77e5099ea8a8048
Michael Pietsch <mp@mpietsch.com>
2024-10-24 20:21:54 +0200

    Initial commit

---
+++ client/src/extension.ts
@@ -0,0 +1,61 @@
+import { workspace, ExtensionContext } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  const configuration = workspace.getConfiguration('tsm-language-server-client');
+
+  const executable = configuration.get<string>('executable');
+  const prefix = configuration.get<string>('prefix');
+  const suggestionsDir = configuration.get<string>('suggestionsdir');
+
+  const runOptions = {
+      command: executable, 
+      args: [
+        "--prefix", prefix,
+        "--suggestionsdir", suggestionsDir,
+      ],
+      transport: TransportKind.stdio
+  }
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    run: runOptions,
+    debug: runOptions
+  };
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for all documents by default
+    documentSelector: [{ scheme: "file", language: "*" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "REPLACE_ME language-server-id",
+    "REPLACE_ME language server name",
+    serverOptions,
+    clientOptions
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
